fa7b7a6c04a97c95f8a6b2af5c08b8dcda31385d
FES001 <firdavs7794948@gmail.com>
2024-08-25 02:21:10 +0500

    s

--- script.js
+++ script.js
@@ -15,3 +15,9 @@ for (let i = 0; i < pagesLinks.length; i++) {
         pagesLinks[i].classList.add('active-page');
     });
 };
+
+// USING UNSPLASH API TO ACCESS PHOTOS
+
+fetch('https://api.unsplash.com/photos/?client_id=qodXyq_d8AThbFoNr29oY1bwI-aeVm6QmGw1KyleNVo')
+    .then(response => response.json())
+    .then(response => console.log(response))
